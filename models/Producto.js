66a7a040afd39dc9693828ca93d095cce2a9bf49
Oscaralcala24 <alum.oalcalae.2020@iesalixar.org>
2023-02-01 16:39:17 +0100

    esquema productos

--- models/Producto.js
+++ models/Producto.js
@@ -5,4 +5,5 @@ var productoEsquema = new Schema({
 nombre: { type: String, required: true },
 tipo: { type: String, required: true },
 precio: { type: Number, required: true },
+imagen: { type: Mixed, required: true },
 });
