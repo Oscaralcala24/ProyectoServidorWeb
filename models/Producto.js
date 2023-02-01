4029357c13e286f497e261e8ef0709b18b12613f
Oscaralcala24 <alum.oalcalae.2020@iesalixar.org>
2023-02-01 16:36:12 +0100

    esquema productos

--- models/Producto.js
+++ models/Producto.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var productoEsquema = new Schema({
-sku: {type:Schema.ObjectId },
 nombre: { type: String, required: true },
 tipo: { type: String, required: true },
 precio: { type: Number, required: true },
