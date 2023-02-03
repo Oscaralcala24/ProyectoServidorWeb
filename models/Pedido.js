c86bea00d7ba08e1ef6232989679db6c6f57eee7
alumjruizr <alum.jruizr.2021@iesalixar.org>
2023-02-03 12:55:49 +0100

    Actualizado 03/02 de verdad

--- models/Pedido.js
+++ models/Pedido.js
@@ -1,1 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var productos = require('./Producto.js');
+var usuarios = require('./Usuario.js');
 
+var pedidoSchema = new Schema({
+    id_pedidos:{type:Schema.ObjectId},
+    fecha:{type: Date , required: true},
+    cantidad:{type: Number , required: true},
+    sku_productos:[{
+        type:Schema.ObjectId,
+        ref: 'Producto'
+    }],
+    id_usuario:[{
+        type: Schema.ObjectId,
+        ref: 'Usuario'
+    }]
+})
+
+module.exports = mongoose.model('Pedido', pedidoSchema);
--- models/Producto.js
+++ models/Producto.js
@@ -1,1 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
+var productoEsquema = new Schema({
+nombre: { type: String, required: true },
+tipo: { type: String, required: true },
+precio: { type: Number, required: true },
+imagen: { type: Mixed, required: true },
+});
+
+module.exports = mongoose.model('usuarios', productoEsquema);
