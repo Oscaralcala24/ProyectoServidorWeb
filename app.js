ccde4702d46cc4786d5e0fe9ac8edd4cc33ce204
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2023-02-03 22:59:47 +0100

    conexion bbbdd

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pedidoRouter = require('./routes/pedido');
+var productoRouter = require('./routes/producto');
+var tiendaRouter = require('./routes/tienda');
+var usuarioRouter = require('./routes/usuario');
 
 var mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
@@ -31,6 +35,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pedido',pedidoRouter);
+app.use('/producto',productoRouter);
+app.use('/tienda',tiendaRouter);
+app.use('/usuario',usuarioRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/usuario.js
+++ routes/usuario.js
@@ -8,3 +8,5 @@ var db = mongoose.connection;
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+module.exports = router;
