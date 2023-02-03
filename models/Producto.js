e869a07a57e0bd25a9674348740bd959bd82d57a
Oscaralcala24 <alum.oalcalae.2020@iesalixar.org>
2023-02-03 22:20:13 +0100

    Creacion de la vista

--- models/Producto.js
+++ models/Producto.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var ProductoSchema = new Schema({
-    nombre: { type: String, required: true },
-    tipo: { type: String, required: true },
-    precio: { type: Number, required: true },
-    imagen: { type: Mixed, required: true },
+var productoEsquema = new Schema({
+nombre: { type: String, required: true },
+tipo: { type: String, required: true },
+precio: { type: Number, required: true },
+imagen: { type: Mixed, required: true },
 });
 
-module.exports = mongoose.model('Producto', ProductoSchema);
+module.exports = mongoose.model("Producto",productoEsquema);
