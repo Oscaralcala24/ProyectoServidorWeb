429f252669565eea4aa2fce26bba9752f8ee9aa7
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2023-02-03 22:45:15 +0100

    as

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+require('dotenv').config()
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
