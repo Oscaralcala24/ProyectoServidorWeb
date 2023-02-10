a1b9c6bc76bd63d84f9ad62bd1ae4299c3913405
Oscar <alum.oalcalae.2020@iesalixar.org>
2023-02-10 15:16:40 +0100

    producto

--- models/Producto.js
+++ models/Producto.js
@@ -3,10 +3,17 @@ var Schema = mongoose.Schema;
 var db = mongoose.connection;
 
 var productoEsquema = new Schema({
+sku: {type: String, required: true},
 nombre: { type: String, required: true },
-tipo: { type: String, required: true },
+stock: { type: Number, required: true },
+talla_ropa: { type: String, required: false },
+talla_zapato: { type: Number, required: false },
 precio: { type: Number, required: true },
 imagen: { type: String, required: true },
+id_categoria:[{
+    type: Schema.ObjectId,
+    ref: 'Categoria'
+}]
 });
 
 module.exports = mongoose.model("Producto",productoEsquema);
--- routes/producto.js
+++ routes/producto.js
@@ -1,12 +1,67 @@
 var express = require('express');
 var router = express.Router();
-var producto = require('../models/Producto')
+var categoria = require('../models/Categoria')
+var Producto = require('../models/Producto')
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 
-/* GET users listing. */
-router.get('/producto', function(req, res, next) {
-  res.send('respond with a resource');
+/* Inserta un producto */
+router.post('/', function(req, res, next) {
+  Producto.create(req.body, function (err, producto) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
 });
+/* Mostrar todos los productos */
+router.get('/', function (req, res, next) {
+  Producto.find(function (err, producto) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(producto);
+  });
+});
+/* Mostrar un producto con una id */
+router.get('/:id', function (req, res, next) {
+  Producto.findById(req.params.id,function (err, producto) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(producto);
+  });
+});
+
+
+/* Mostrar todos los productos de una categoría */
+router.get('/categoria/:id_categoria', function (req, res, next) {
+  Pedido.aggregate({},[{$lookup:{from:"categorias",localField:"id_categoria",foreignField:"id_categoria",as:"categoria_id"}}],function (err,producto) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200).json(producto);
+  })
+});
+
+
+/* Borrar un producto */
+router.delete('/:id', function (req, res, next) {
+  Producto.findByIdAndDelete(req.params.id, function (err, producto) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+
+/* Modificar stock producto */
+router.put('/:id', function (req, res, next) {
+  Producto.findByIdAndUpdate(req.params.id, req.body, function (err) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+
+
+/* Muestra los productos favoritos del usuario */
+router.get('/categoria/:id_categoria', function (req, res, next) {
+  Producto.findById(req.params.id,function (err, producto) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(producto);
+  });
+});
+
+
 
 module.exports = router;
