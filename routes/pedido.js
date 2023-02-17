8f0cc0cc079ad36149321f9f5ead82b0e3ea803c
unknown <you@example.com>
2023-02-17 14:36:37 +0100

    upgrade query pedido

--- routes/pedido.js
+++ routes/pedido.js
@@ -22,16 +22,50 @@ router.post('/', function (req, res, next) {
   });
 });
 
+/*GET - Muestra los pedidos ordenados por una fecha determinada [ASC]*/
+router.get('/ordenarFechaASC',function (req, res) {
+  let fecha = req.body.fecha;
+  
+  if (fecha !== undefined && fecha !== null){
+    Pedido.find().where('fecha').gte(fecha).exec(function (err, estadoPedido) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(estadoPedido);
+    })
+  }else{
+    Pedido.find().exec(function (err, estadoPedido) { //Si no lo encuentra nos mostrará un array vacio, y si la query está vacia todos los pedidos.
+      if (err) res.status(500).send(err);
+      else res.status(200).json(estadoPedido);
+    });
+  }
+});
+
+/*GET - Muestra los pedidos ordenados por una fecha determinada [DESC]*/
+router.get('/ordenarFechaASC',function (req, res) {
+  let fecha = req.body.fecha;
+  
+  if (fecha !== undefined && fecha !== null){
+    Pedido.find().where('fecha').lte(fecha).exec(function (err, estadoPedido) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(estadoPedido);
+    })
+  }else{
+    Pedido.find().exec(function (err, estadoPedido) { //Si no lo encuentra nos mostrará un array vacio, y si la query está vacia todos los pedidos.
+      if (err) res.status(500).send(err);
+      else res.status(200).json(estadoPedido);
+    });
+  }
+});
+
 /*GET - Muestra los estados de los pedidos, pasando por url el estado que quiere visualizar*/
 router.get('/',function (req,res,next) {
-  let queryPedido = req.query.estado;
-  if(queryPedido !== null){
-    Pedido.find({ estado: queryPedido }).exec(function (error, estadoInfo) {
+  let queryPedido = req.query.estado; //Creamos la query, con el estado que hemos pasado por parametro
+  if(queryPedido !== undefined){ //Si la query no está vacia nos entrará dentro del if
+    Pedido.find({ estado: queryPedido }).exec(function (error, estadoInfo) { //Buscara los estados, que le hemos pasado por la query y si el estado es 200 nos creara un json con la respuesta.
       if (error) res.status(500).send(error);
       else res.status(200).json(estadoInfo);
     });
   }else{
-    Pedido.find().exec(function (error, estadoInfo) {
+    Pedido.find().exec(function (error, estadoInfo) { //Si no lo encuentra nos mostrará un array vacio, y si la query está vacia todos los pedidos.
       if (error) res.status(500).send(error);
       else res.status(200).json(estadoInfo);
     });
@@ -54,7 +88,5 @@ router.delete('/:id', function (req, res, next) {
     else res.sendStatus(200);
   });
 });
-
-
 
 module.exports = router;
