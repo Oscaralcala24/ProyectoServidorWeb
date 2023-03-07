8544c725e33bfc97efd61d9ac351fbf8ed212975
Daniel <silverhawkdeveloper@gmail.com>
2023-03-07 12:25:33 +0100

    vista Usuario

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 require('dotenv').config()
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const { body,validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 var indexRouter = require('./routes/index');
 var pedidoRouter = require('./routes/pedido');
@@ -17,8 +17,8 @@ var mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
-.then(() => console.log('connection successful'))
-.catch((err) => console.error(err));
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.error(err));
 
 var db = mongoose.connection;
 
@@ -30,50 +30,50 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true })); // Cambiado a true
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/pedido',pedidoRouter);
-app.use('/producto',productoRouter);
-app.use('/VarianteProducto',varianteProducto);
-app.use('/usuario',usuarioRouter);
+app.use('/pedido', pedidoRouter);
+app.use('/producto', productoRouter);
+app.use('/VarianteProducto', varianteProducto);
+app.use('/usuario', usuarioRouter);
 
 /*Express-Validator-Pedido*/
 app.get('/express-validatorPedido', function (req, res) {
   res.render('crearPedido')
 })
 
-app.post('/registrarPedido',[
-  body('estado','Ingrese un estado Correcto').isEmpty().exists(),
-  body('fecha','Ingrese una fecha Correcta').isDate(),
-  body('cantidad','Ingresa una cantidad por favor').exists().isNumeric(),
-  body('Producto','Ingresa el id de un producto por favor').exists(),
-  body('usuario','Ingresa el id de un usuario por favor').exists()
-],(req, res) => {
+app.post('/registrarPedido', [
+  body('estado', 'Ingrese un estado Correcto').isEmpty().exists(),
+  body('fecha', 'Ingrese una fecha Correcta').isDate(),
+  body('cantidad', 'Ingresa una cantidad por favor').exists().isNumeric(),
+  body('Producto', 'Ingresa el id de un producto por favor').exists(),
+  body('usuario', 'Ingresa el id de un usuario por favor').exists()
+], (req, res) => {
   const errors = validationResult(req);
   // if(!errors.isEmpty()){ //NOS GUARDA EN UN ARRAY TODOS LOS ERRORES. NOS LO GUARDA EN EL ARRAY ERRORS
   //   return res.status(400).json({errors: errors.array()});
   // }
 
-  if(!errors.isEmpty()){
+  if (!errors.isEmpty()) {
     console.log(req.body)
     const valores = req.body
     const validaciones = errors.array()
-    res.render('crearPedido.ejs',{validaciones:validaciones,valores:valores})
-  }else{
+    res.render('crearPedido.ejs', { validaciones: validaciones, valores: valores })
+  } else {
     res.send('!Validacion Correcta¡')
   }
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/usuario.js
+++ routes/usuario.js
@@ -4,6 +4,12 @@ var usuario = require('../models/Usuario')
 var producto = require('../models/Producto')
 var mongoose = require('mongoose');
 var db = mongoose.connection;
+const { body, validationResult } = require('express-validator');
+
+// Render 
+router.get('/', function (req, res, next) {
+  res.render('usuario');
+});
 
 // GET - Listado de usuarios ordenados por fecha de creación
 router.get('/', function (req, res, next) {
@@ -48,13 +54,46 @@ router.post('/findusuario', function (req, res, next) {
   });
 });
 
-// POST - Crear un nuevo usuario
-router.post('/', function (req, res, next) {
-  usuario.create(req.body, function (error, usuarioInfo) {
-    if (error) res.status(500).send(error);
-    else res.sendStatus(200);
+// POST - Crear un nuevo usuario y validación lado servidor
+router.post('/registrarUsuario', [
+  body('dni', 'Introduzca su DNI')
+    .exists() // Que no esté vacio
+    .isLength({ min: 9 }), // Longitud caracteres
+  body('nombre', 'Introduzca su nombre')
+    .exists()
+    .isLength({ min: 3 }),
+  body('apellidos', 'Introduzca sus apellidos')
+    .exists()
+    .isLength({ min: 3 }),
+  body('email', 'Introduzca su email')
+    .exists()
+    .isEmail(), // Email valido
+  body('telefono', 'Introduzca su telefono')
+    .exists()
+    .isNumeric() // Número valido
+    .isLength({ min: 9, max: 9 }),
+  body('direccion', 'Introduzca su dirección')
+    .exists()
+    .isLength({ min: 3 }),
+  body('contrasenia', 'Introduzca su contraseña')
+    .exists()
+    .isLength({ min: 3 }),
+],
+  function (req, res) {
+    const errorValidacion = validationResult(req);
+    if (!errorValidacion.isEmpty()) {
+      //return res.status(400).json({ errorValidacion: errorValidacion.array() });
+      const validaciones = errorValidacion.array();
+      const valores = req.body;
+      //console.log(validaciones);
+      res.render('usuario', {validaciones:validaciones, valores:valores});
+    } else {
+      usuario.create(req.body, function (error, usuarioInfo) {
+        if (error) res.status(500).send(error);
+        else res.sendStatus(200);
+      });
+    }
   });
-});
 
 // POST - Comprueba si el usuario existe
 router.post('/signin', function (req, res, next) {
